be4120f0933389e9585873e18f4a797ab521de03
Isadora Sozo <134089736+Isa-Sozo@users.noreply.github.com>
2024-11-01 09:45:09 -0300

    Add files via upload

---
+++ atividade array/2°Algoritimo.js
@@ -0,0 +1,15 @@
+/*Desenvolva um aplicativo de lista de compras automatizada. Crie um array vazio chamado 'listaCompras'. 
+Solicite ao usuário para adicionar três itens à lista. Verifique se o último item adicionado é "leite". 
+Se não for, adicione "leite" ao final da lista. Imprima a lista de compras no final.*/
+
+let listaCompras = [];
+
+listaCompras.push(prompt("Adicione itens para sua lista de compras."))
+listaCompras.push(prompt("Adicione itens para sua lista de compras."))
+listaCompras.push(prompt("Adicione itens para sua lista de compras."))
+
+if(listaCompras[2] != "leite"){
+    listaCompras.push("leite")
+}
+
+console.log(listaCompras);
